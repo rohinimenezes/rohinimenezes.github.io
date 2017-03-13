d6d67780452a56da6be29b5bb3a2d8d1aef595a7
Rohini Menezes <rmenezes2017@kellogg.northwestern.edu>
2017-03-12 22:28:30 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,123 @@
+// React
+var React = require('react')
+var ReactDOM = require('react-dom')
+
+// Google Maps
+var ReactGMaps = require('react-gmaps')
+var {Gmaps, Marker} = ReactGMaps
+
+// Movie data
+var movieData = require('./data/movies.json')
+var theatres = require('./data/theatres.json')
+
+// Components
+var Header = require('./components/Header')
+var MovieDetails = require('./components/MovieDetails')
+var MovieList = require('./components/MovieList')
+var NoCurrentMovie = require('./components/NoCurrentMovie')
+var SortBar = require('./components/SortBar')
+var TheatreMap = require ('./components/TheatreMap')
+
+// There should really be some JSON-formatted data in movies.json, instead of an empty array.
+// I started writing this command to extract the data from the learn-sql workspace
+// on C9, but it's not done yet :) You must have the csvtojson command installed on your
+// C9 workspace for this to work.
+// npm install -g csvtojson
+// sqlite3 -csv -header movies.sqlite3 'select "imdbID" as id, "title" from movies' | csvtojson --maxRowLength=0 > movies.json
+
+// Firebase configuration
+var Rebase = require('re-base')
+var base = Rebase.createClass({
+  apiKey: " AIzaSyCDjJJcoyUHY27BdlxPX2_AyPTirOlgCEs",   // replace with your Firebase application's API key
+  databaseURL: "https://buyflix-d348b.firebaseapp.com/",
+})
+
+var App = React.createClass({
+  movieClicked: function(movie) {
+    this.setState({
+      currentMovie: movie
+    })
+  },
+  movieWatched: function(movie) {
+    var existingMovies = this.state.movies
+    var moviesWithWatchedMovieRemoved = existingMovies.filter(function(existingMovie) {
+      return existingMovie.id !== movie.id
+    })
+    this.setState({
+      movies: moviesWithWatchedMovieRemoved,
+      currentMovie: null
+    })
+  },
+  resetMovieListClicked: function() {
+    this.setState({
+      movies: movieData.sort(this.movieCompareByReleased)
+    })
+  },
+  viewChanged: function(view) {
+    this.setState({
+      currentView: view
+    })
+  },
+  renderMovieDetails: function() {
+    if (this.state.currentMovie == null) {
+      return <NoCurrentMovie resetMovieListClicked={this.resetMovieListClicked} />
+    } else {
+      return <MovieDetails movie={this.state.currentMovie}
+                           movieWatched={this.movieWatched} />
+    }
+  },
+  renderMainSection: function() {
+    if (this.state.currentView === 'map') {
+      return ( <TheatreMap />
+      )
+    } else {
+      return (
+        <div>
+          <MovieList movies={this.state.movies} movieClicked={this.movieClicked} />
+          {this.renderMovieDetails()}
+        </div>
+      )
+    }
+  },
+  movieCompareByTitle: function(movieA, movieB) {
+    if (movieA.title < movieB.title) {
+      return -1
+    } else if (movieA.title > movieB.title) {
+      return 1
+    } else {
+      return 0
+    }
+  },
+  movieCompareByReleased: function(movieA, movieB) {
+    if (movieA.released > movieB.released) {
+      return -1
+    } else if (movieA.released < movieB.released) {
+      return 1
+    } else {
+      return 0
+    }
+  },
+  getInitialState: function() {
+    return {
+      movies: movieData.sort(this.movieCompareByReleased),
+      currentMovie: null,
+    }
+  },
+  componentDidMount: function() {
+    base.syncState('/movies',
+    { context: this, state: 'movies', asArray: true })
+  },
+  render: function() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <SortBar movieCount={this.state.movies.length} viewChanged={this.viewChanged} />
+        <div className="main row">
+          {this.renderMainSection()}
+        </div>
+      </div>
+    )
+  }
+})
+
+ReactDOM.render(<App />, document.getElementById("app"))
